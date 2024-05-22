6aac1d6cb03e647076506e99dde90e681dd7bbb0
RISHABH MISHRA <92362688+R1SH4BH81@users.noreply.github.com>
2024-05-22 20:57:39 +0530

    Add files via upload

---
+++ assets/js/readTime.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the article content
+  var articleContent =
+    document.getElementById("article-content").innerText;
+
+  // Average reading speed (words per minute)
+  var wordsPerMinute = 170;
+
+  // Calculate the read time
+  var words = articleContent.split(/\s+/).length;
+  var minutes = words / wordsPerMinute;
+  var readTime = Math.ceil(minutes);
+
+  // Update the HTML with the read time estimate
+  var readTimeElement = document.getElementById("read-time");
+  readTimeElement.textContent =
+    "read time: " + readTime + " min(s)";
+});
